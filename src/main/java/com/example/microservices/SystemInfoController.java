bb6d2a8f1a17837f1584308bffbf42765b057e27
Kishan <kishanjvns@gmail.com>
2024-10-19 12:27:58 +0530

    Create SystemInfoController.java

---
+++ src/main/java/com/example/microservices/SystemInfoController.java
@@ -0,0 +1,32 @@
+package com.example.systeminfo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class SystemInfoController {
+
+    @GetMapping("/api/system-info")
+    public Map<String, String> getSystemInfo() throws UnknownHostException {
+        Map<String, String> systemInfo = new HashMap<>();
+
+        InetAddress localHost = InetAddress.getLocalHost();
+        String ipAddress = localHost.getHostAddress();
+        String hostName = localHost.getHostName();
+
+        systemInfo.put("IP Address", ipAddress);
+        systemInfo.put("Host Name", hostName);
+        systemInfo.put("Canonical Host Name", localHost.getCanonicalHostName());
+        systemInfo.put("Operating System", System.getProperty("os.name"));
+        systemInfo.put("OS Version", System.getProperty("os.version"));
+        systemInfo.put("OS Architecture", System.getProperty("os.arch"));
+        systemInfo.put("User", System.getProperty("user.name"));
+
+        return systemInfo;
+    }
+}
